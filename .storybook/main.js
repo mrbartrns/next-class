2f636701576ced99415da4f29672a0a0408ef264
Junhyuck Ko <mrbartrns@naver.com>
2022-06-27 11:15:34 +0900

    [chore] add packages

---
+++ .storybook/main.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config) => {
+    const alias = {
+      '@': path.resolve(__dirname, '../src'),
+      '@components': path.resolve(__dirname, '../src/components'),
+      '@hooks': path.resolve(__dirname, '../src/hooks'),
+      '@contexts': path.resolve(__dirname, '../src/contexts'),
+      '@pages': path.resolve(__dirname, '../src/pages'),
+    };
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      ...alias,
+    };
+    return config;
+  },
+};
