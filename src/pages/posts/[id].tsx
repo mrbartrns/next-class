75c2753c0db98c394f7f9a40b2394be30e67151c
Junhyuck Ko <mrbartrns@naver.com>
2022-06-27 14:22:25 +0900

    [feat] practice pre-fetch (ssr)

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -10,12 +10,24 @@ interface Props {
 
 // ANCHOR: context가 필요하다.
 export const getServerSideProps = async (ctx: NextPageContext) => {
-  const postId = ctx.query.id;
-  const { data: post } = await axios.get<PostInterface>(
-    `https://jsonplaceholder.typicode.com/posts/${postId}`,
-  );
+  try {
+    const postId = ctx.query.id;
+    const { data: post } = await axios.get<PostInterface>(
+      `https://jsonplaceholder.typicode.com/posts/${postId}`,
+    );
+    return {
+      props: { post },
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response?.status === 404) {
+      // ANCHOR: not found시 404page로 자동으로 이동한다.
+      return {
+        notFound: true,
+      };
+    }
+  }
   return {
-    props: { post },
+    props: {},
   };
 };
 
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,6 +3,10 @@ import Link from 'next/link';
 import axios from 'axios';
 import { Post } from '@/interfaces';
 
+/**
+ * ANCHOR: 서버로부터 데이터를 받아와 템플릿을 구성할 경우
+ * 초기 로딩 시간이 훨씬 빠르다
+ */
 export const getServerSideProps = async () => {
   const { data: posts } = await axios.get(
     'https://jsonplaceholder.typicode.com/posts',
@@ -23,7 +27,9 @@ const Posts: React.FC<Props> = ({ posts }) => {
         {posts.map((post) => {
           return (
             <li key={post.id}>
-              <Link href={`/posts/${post.id}`}>{post.title}</Link>
+              <Link href="/posts/[id]" as={`/posts/${post.id}`}>
+                {post.title}
+              </Link>
             </li>
           );
         })}
