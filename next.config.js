f535a08987d45de38992fda7882a35b637c8917b
Junhyuck Ko <mrbartrns@naver.com>
2022-06-27 10:10:19 +0900

    [chore] absolute path settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
+/* eslint-disable no-param-reassign */
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.resolve = {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@hooks': path.resolve(__dirname, 'src/hooks'),
+        '@contexts': path.resolve(__dirname, 'src/contexts'),
+        '@pages': path.resolve(__dirname, 'src/pages'),
+        '@styles': path.resolve(__dirname, 'src/styles'),
+      },
+      ...config.resolve,
+    };
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
