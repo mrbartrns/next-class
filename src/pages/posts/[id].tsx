0917facd2153e978e19ff186f92a70330bdb9130
Junhyuck Ko <mrbartrns@naver.com>
2022-06-27 11:15:43 +0900

    [feat] next basic

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -3,7 +3,19 @@ import React from 'react';
 
 const Post: React.FC = () => {
   const router = useRouter();
-  return <div>Post {router.query.id}</div>;
+  return (
+    <div>
+      Post {router.query.id}
+      <button
+        onClick={() => {
+          router.push('/about');
+        }}
+        type="button"
+      >
+        About으로 이동하기
+      </button>
+    </div>
+  );
 };
 
 export default Post;
