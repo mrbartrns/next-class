b05bbc8cdbe2e5fa1c7b827c70a7eca39cc7181b
Junhyuck Ko <mrbartrns@naver.com>
2022-07-17 22:45:39 +0900

    [feat] Navbar component 구성

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav>
+      <Link href="/">Home</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
