5264e7373655695426a643afdc1e28bfbbf25d7f
Junhyuck Ko <mrbartrns@naver.com>
2022-07-17 22:48:47 +0900

    [fix] about page navbar 제거

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Navbar from '@components/Navbar';
 
 const About: React.FC = () => {
   return (
     <div>
       <h1>About Page</h1>
-      <Navbar />
     </div>
   );
 };
