0da98db45ef3f4c077a31e304933f5663486a03b
Junhyuck Ko <mrbartrns@naver.com>
2022-07-17 22:45:20 +0900

    [feat] about page 생성

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '@components/Navbar';
+
+const About: React.FC = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <Navbar />
+    </div>
+  );
+};
+
+export default About;
